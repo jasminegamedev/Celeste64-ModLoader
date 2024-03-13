5e232db1eae90635524ff20966c6ca318d362dd4
psyGamer <psygamer21@gmail.com>
2024-03-13 22:13:20 +0100

    sort modifiers

--- Source/Helpers/StateMachine.cs
+++ Source/Helpers/StateMachine.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Celeste64;
 
-public unsafe sealed class StateMachine<TIndex, TEvent>
+public sealed unsafe class StateMachine<TIndex, TEvent>
 	where TIndex : unmanaged, Enum
 	where TEvent : unmanaged, Enum
 {
