2de233597689cd5a53195369bef22fac57655ad6
Axi <98420477+axiand@users.noreply.github.com>
2024-03-14 18:39:54 +0100

    Hide prompt/title when a level is selected

--- Source/Scenes/Overworld.cs
+++ Source/Scenes/Overworld.cs
@@ -536,11 +536,11 @@ public class Overworld : Scene
 			var staticPromptPos = bounds.TopCenter + new Vec2(1, 16) * Game.RelativeScale;
 			var modTitlePos = bounds.TopCenter + new Vec2(-8 + (-slide * 160), 32) * Game.RelativeScale;
 
-			if(selectedModIdx == 0)
+			if(selectedModIdx == 0 && state == States.Selecting)
 			{
 				UI.Text(batch, new Loc.Localized("FujiOverworldModSlideNote"), staticPromptPos, new Vec2(0.5f, 0), Color.Gray);
 			}
-			else
+			else if(state == States.Selecting)
 			{
 				UI.Text(batch, modsWithLevels[selectedModIdx].ModInfo.Name, modTitlePos, new Vec2(0.5f, 0), Color.White);
 			}
