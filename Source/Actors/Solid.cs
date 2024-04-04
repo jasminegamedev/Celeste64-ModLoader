60988954ef46f556447945e9a8147efbec655fe7
psyGamer <psygamer21@gmail.com>
2024-04-04 11:48:59 +0200

    Generalize GeometryDefinition

--- Source/Actors/Solid.cs
+++ Source/Actors/Solid.cs
@@ -7,33 +7,12 @@ namespace Celeste64;
 
 public class Solid : Actor, IHaveModels
 {
-	public class Definition : ActorDefinition
+	public class Definition : GeometryDefinition
 	{
+		protected override Matrix Transform => Matrix.CreateTranslation(Position);
+		
 		[SpecialProperty(SpecialPropertyType.PositionXYZ)]
 		public Vec3 Position { get; set; }
-		
-		static float size => 10.0f;
-
-		public List<Vec3> Vertices { get; set; } = [
-			new Vec3(-size, size, size), new Vec3(size, size, size), new Vec3(size, size, -size), new Vec3(-size, size, -size),
-			new Vec3(-size, -size, size), new Vec3(size, -size, size), new Vec3(size, -size, -size), new Vec3(-size, -size, -size)
-		];
-		
-		public List<List<int>> Faces { get; set; } = [
-			[0, 1, 2, 3], // Front
-			[7, 6, 5, 4], // Back
-			[4, 5, 1, 0], // Top
-			[3, 2, 6, 7], // Bottom
-			[1, 5, 6, 2], // Left
-			[4, 0, 3, 7], // Right
-		];
-		
-		public Definition()
-		{
-			SelectionTypes = [
-				new VertexSelectionType(this),
-			];
-		}
 		
 		public override Actor[] Load(World.WorldType type)
 		{
@@ -97,60 +76,6 @@ public class Solid : Actor, IHaveModels
 			solid.Position = Position + bounds.Center;
 			
 			return [solid];
-		}
-	}
-	
-	public class VertexSelectionType(Solid.Definition def) : SelectionType
-	{
-		private readonly List<SelectionTarget> targets = [];
-		// TODO: Support other gizmos depending on current tool. Maybe with some restriction on which tools are allowed tho
-		private PositionGizmo? vertexGizmo = null;
-		
-		public override IEnumerable<SelectionTarget> Targets => targets;
-		public override IEnumerable<Gizmo> Gizmos => vertexGizmo is null ? [] : [vertexGizmo];
-
-		public override void Awake()
-		{
-			EditorWorld.Current.OnTargetSelected += target =>
-			{
-				// Deselect if something else was selected
-				if (target is null || !targets.Contains(target) && !(vertexGizmo?.SelectionTargets.Contains(target) ?? false))
-					vertexGizmo = null;
-			};
-			
-			def.OnUpdated += () =>
-			{
-				targets.Clear();
-				// TODO: Detect when geometry changed?
-				// vertexGizmo = null;
-				
-				var transform = Matrix.CreateTranslation(def.Position);
-				const float selectionRadius = 1.0f;
-
-				foreach (var face in def.Faces)
-				{
-					foreach (int idx in face)
-					{
-						targets.Add(new SimpleSelectionTarget(transform, new BoundingBox(def.Vertices[idx], selectionRadius * 2.0f))
-						{
-							// OnHovered = () => Log.Info($"Hovered vertex {vertex}"),
-							OnSelected = () =>
-							{
-								Log.Info($"Selected vertex {idx} {def.Vertices[idx]}");
-								vertexGizmo = new PositionGizmo(
-									() => def.Vertices[idx] + def.Position,
-									v =>
-									{
-										def.Vertices[idx] = v - def.Position;
-										def.Dirty = true;
-									},
-									scale: 0.5f);
-							},
-							// OnDragged = (mouseDelta, mouseRay) => Log.Info($"Dragged vertex {vertex} ({mouseDelta}, {mouseRay})"),
-						});
-					}
-				}
-			};
 		}
 	}
 	
